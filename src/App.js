d1873a5cd407452628a942a1dfb627b086831aec
Gcolin1 <guilhermecolin09@gmail.com>
2023-05-23 14:52:35 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Notebook from './assets/notebook.png'
+import Instagram from './assets/instagram.png'
+import Github from './assets/github.png'
+import Linkedin from './assets/linkedin.png'
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <header>
+            <nav className='menu'>
+                <ul>
+                    <a href="#sobre"><li>Sobre mim</li></a>
+                    <a href="#conhecimentos"><li>Conhecimentos</li></a>
+                    <a href="#projetos"><li>Projetos</li></a>
+                    <a href="#contato"><li>Contato</li></a>
+                </ul>
+            </nav>
+
+            <section className="container_image">
+                <div className="info">
+                    <div>
+                        <h1>Guilherme Nascimento Colin</h1>
+                        <h2>Desenvolvedor web</h2>
+                    </div>
+                    <img src={Notebook} alt="notebook imagem" />
+                </div>
+            </section>
+
+            <div className="links">
+                <a href="https://www.instagram.com/guilhermecolin1/" target="_blank" ><img src={Instagram} alt="Instagram logo" /></a>
+                <a href="https://www.linkedin.com/in/guilherme-nascimento-colin-684a50199" target="_blank"><img src={Linkedin} alt="linkedin logo" /></a>
+                <a href="https://github.com/Gcolin1" target="_blank"><img src={Github} alt="github logo" /></a>
+            </div>
+        </header>
     </div>
   );
 }
