c776cf2a402445e1b7ec8a99f5e4c2cf362cdffd
Gcolin1 <guilhermecolin09@gmail.com>
2023-05-24 23:51:01 -0300

    skills

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ function App() {
                         <div className="vertical_line"></div>
 
                         <div className="experiencia">
-                            <h3>Análista de suporte técnico l - TOTVS <br></br> 04/2023 (o momento)</h3>
+                            <h3>Análista de suporte técnico l - TOTVS 04/2023 (o momento)</h3>
                             <p>Prestar suporte a clientes da empresa no sistema totvs food service</p>
                         </div>
 
@@ -76,6 +76,81 @@ function App() {
 
                     <div className='profissional'>
                         <h2>Formação academica</h2>
+
+                        <div className="experiencia">
+                            <h3>Ensino médio - Darcy Ribeiro <br></br> 02/2017 a 12/2019</h3>
+                        </div>
+
+                        <div className="vertical_line"></div>
+
+                        <div className="experiencia">
+                            <h3>Tecnico em informatica - SENAC <br></br> 02/2021 a 07/2022</h3>
+                        </div>
+
+                        <div className="vertical_line"></div>
+
+                        <div className="experiencia">
+                            <h3>Programação Web - IOS<br></br> 04/2023 (o momento)</h3>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="container_skills">
+                <div className='site container-hard'>
+                    <div className='hardskills'>
+                        <h2>Hard Skills</h2>
+                            <div className="conhecimento">
+                                <h3>HTML</h3>
+                                <span>70%</span>
+                                <div className="container_percentual"></div>
+                                <div className="percentual html-bar" ></div>
+                            </div>
+                            <div className="conhecimento">
+                                <h3>CSS</h3>
+                                <span>75%</span>
+                                <div className="container_percentual"></div>
+                                <div className="percentual css-bar" ></div>
+                            </div>
+                            <div className="conhecimento">
+                                <h3>Javascript</h3>
+                                <span>70%</span>
+                                <div className="container_percentual"></div>
+                                <div className="percentual js-bar" ></div>
+                            </div>
+                            <div className="conhecimento">
+                                <h3>React</h3>
+                                <span>50%</span>
+                                <div className="container_percentual"></div>
+                                <div className="percentual react-bar" ></div>
+                            </div>
+                            <div className="conhecimento">
+                                <h3>Angular</h3>
+                                <span>50%</span>
+                                <div className="container_percentual"></div>
+                                <div className="percentual angular-bar" ></div>
+                            </div>
+                            <div className="conhecimento">
+                                <h3>SQL/MYSQL/MSSQL</h3>
+                                <span>60%</span>
+                                <div className="container_percentual"></div>
+                                <div className="percentual sql-bar" ></div>
+                            </div>
+                            <div className="conhecimento">
+                                <h3>JAVA</h3>
+                                <span>40%</span>
+                                <div className="container_percentual"></div>
+                                <div className="percentual java-bar" ></div>
+                            </div>
+                            <div className="conhecimento">
+                                <h3>Springboot</h3>
+                                <span>30%</span>
+                                <div className="container_percentual"></div>
+                                <div className="percentual spring-bar" ></div>
+                            </div>
+                        </div>
+                    <div className="softskills">
+                        <h2>Soft skills</h2>
                     </div>
                 </div>
             </section>
